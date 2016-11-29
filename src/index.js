2fdab538171632f0f38ffdee9b7351a7c5398ea6
Andrew Song <andrewgnos@gmail.com>
2016-11-28 21:29:40 -0800

    add base routing

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
-import App from './App'
+import { render } from 'react-dom'
+import Routes from './Routes'
 import './index.css'
 
-ReactDOM.render(
-  <App />,
+render(
+  <Routes />,
   document.getElementById('root'),
 )
