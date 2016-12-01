091954269c192b2b440e3bf96e168f7f18ea9ee9
Andrew Song <andrewgnos@gmail.com>
2016-11-30 23:49:06 -0800

    base redux

--- src/Routes.js
+++ src/Routes.js
@@ -1,11 +1,14 @@
-import React from 'react'
+import React, { PropTypes } from 'react'
 import { Router, Route, IndexRoute, browserHistory } from 'react-router'
+import { Provider } from 'react-redux'
 import App from './App'
 
-const Routes = () => (
-  <Router history={browserHistory}>
-    <Route path="/" component={App} />
-  </Router>
+const Routes = ({ store }) => (
+  <Provider store={store}>
+    <Router history={browserHistory}>
+      <Route path="/" component={App} />
+    </Router>
+  </Provider>
 )
 
 export default Routes
--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 import React from 'react'
+import { createStore } from 'redux'
 import { render } from 'react-dom'
 import Routes from './Routes'
 import './index.css'
 
+// change anon function to your main reducer
+const store = createStore(() => {}, {})
+
 render(
-  <Routes />,
+  <Routes store={store} />,
   document.getElementById('root'),
 )
