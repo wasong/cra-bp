18d059aacce798fe94765c70c1e0428da074b38d
Andrew Song <andrewgnos@gmail.com>
2017-01-04 17:21:38 -0800

    boilerplate reducers/actions and redux-logger, thunk

---
+++ src/actions/actions.js
@@ -0,0 +1,7 @@
+export const DEFAULT_ACTION = 'DEFAULT_ACTION'
+
+export const defaultAction = () => {
+  return {
+    type: DEFAULT_ACTION,
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,11 +1,28 @@
+import injectTapEventPlugin from 'react-tap-event-plugin'
 import React from 'react'
-import { createStore } from 'redux'
+import { createStore, applyMiddleware, combineReducers } from 'redux'
 import { render } from 'react-dom'
+import thunkMiddleware from 'redux-thunk'
+import createLogger from 'redux-logger'
+
+import rootReducer from './reducers/reducers'
 import Routes from './Routes'
 import './index.css'
 
-// change anon function to your main reducer
-const store = createStore(() => {}, {})
+// TODO: babel-polyfill, redux-thunk, redux-logger
+injectTapEventPlugin()
+
+const loggerMiddleware = createLogger()
+
+const store = createStore(rootReducer,
+  {
+    base: '',
+  },
+  applyMiddleware(
+    thunkMiddleware,
+    loggerMiddleware,
+  ),
+)
 
 render(
   <Routes store={store} />,
